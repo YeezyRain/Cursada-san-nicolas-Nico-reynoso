54174498c197d7694c61518d5abdd88ffba02390
YeezyRain <109553271+YeezyRain@users.noreply.github.com>
2023-09-06 18:32:05 -0300

    aaaaaaaaaaa

--- js/site.js
+++ js/site.js
@@ -1,21 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("comprobarEdad").addEventListener("click", function () {
+
+      const edad = document.getElementById("edadInput").value;
+    
+    
+      if (!isNaN(edad) && edad !== "") {
+    
+          edad = parseInt(edad);
+    
+    
+          if (edad >= 18) {
+              alert("Eres mayor de edad. Puedes continuar.");
+          } else {
+              alert("Eres menor de edad. No puedes continuar.");
+          }
+          } else {
+          alert("Por favor, introduce una edad válida.");
+          }
+    });
+  });
+  
 console.log ("hola gente")
-
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-const appendAlert = (message, type) => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
-
-  alertPlaceholder.append(wrapper)
-}
-
 const alertTrigger = document.getElementById('liveAlertBtn')
 if (alertTrigger) {
   alertTrigger.addEventListener('click', () => {
     appendAlert('Recuerda beber agua', 'success')
   })
 }
+
